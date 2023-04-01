3f8cc99b0214941d44b39e7d7669bb76be78b516
KIM <KIM@Kimng.mshome.net>
2023-04-01 23:52:17 +0900

    import_first

---
+++ src/com/greedy/sub/Application.java
@@ -0,0 +1,42 @@
+package com.greedy.sub;
+import com.greedy.sub.*;
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		System.out.println("name?");
+		System.out.println("~serching~");
+		
+		name subName = new name();
+		subName.subSumName();
+		
+		System.out.println("name done");
+		
+		
+		System.out.println("age?");
+		System.out.println("~serching~");
+		
+		age subAge = new age();
+		subAge.subSumAge();
+		
+		System.out.println("age done");
+		
+		System.out.println("email?");
+		System.out.println("~name serching~");
+		System.out.println("~email serching~");
+		
+		email subEmail = new email();
+		subEmail.subSumEmail();
+				
+		System.out.println("done");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/com/greedy/sub/email.java
@@ -0,0 +1,28 @@
+package com.greedy.sub;
+import com.greedy.sub.*;
+
+public class email {
+
+	public static void main(String[] args) {
+		
+		name subname = new name();
+		subname.subSumName();	
+	}
+	
+	public String subSumEmail() {
+		
+		String email1 = "k@gmail.com";
+		String email2 = "n@gmail.com";
+		String email3 = "g@gmail.com";
+		
+		System.out.println(email1);
+		System.out.println(email2);
+		System.out.println(email3);
+	
+		return null;
+		
+	}
+
+	
+	
+}
