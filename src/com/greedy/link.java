c39de052c6491b152cc1086d1e45f0cf4f10dc79
kimng1003 <kimnagyeong50@gmail.com>
2023-04-01 22:19:10 +0900

    method1 commit

---
+++ src/com/greedy/link.java
@@ -0,0 +1,17 @@
+package com.greedy;
+
+import com.greedy.*;
+
+public class link {
+
+	public static void main(String[] args) {
+		
+		test1 call = new test1();
+		call.name();
+		call.age();
+		call.email();
+		
+	}
+	
+	
+}
